6431b9ddb67f76a0d7dbfd58a1aac8382413cb5f
Josmar Salvador <josmarmp96@gmail.com>
2021-05-27 19:56:57 -0500

    Login: Fix in if condition

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -15,24 +15,24 @@ class AuthController extends Controller
     public function register( Request $request ) {
 
         $fields = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|unique:users,email',
-            'password' => 'required|string|confirmed',
+            'name'      => 'required|string',
+            'email'     => 'required|string|unique:users,email',
+            'password'  => 'required|string|confirmed',
         ]);
 
         $user = User::create([
-            'name' => $fields['name'],
-            'email' => $fields['email'],
-            'password' => Hash::make( $fields['password'] ),
+            'name'      => $fields['name'],
+            'email'     => $fields['email'],
+            'password'  => Hash::make( $fields['password'] ),
         ]);
 
         $token = $user->createToken('authtoken')->plainTextToken;
 
         return response()->json([
-            'title' => 'Usuario creado correctamente',
-            'message' => 'Bienvenido ' . $user->name,
-            'user' => $user,
-            'token' => $token,
+            'ok'        => true,
+            'message'   => 'Bienvenido ' . $user->name,
+            'user'      => $user,
+            'token'     => $token,
         ], 201);
 
     }
@@ -41,18 +41,25 @@ class AuthController extends Controller
     public function login( Request $request ) {
 
         $fields = $request->validate([
-            'email' => 'required|string',
-            'password' => 'required|string',
+            'email'     => 'required|string',
+            'password'  => 'required|string',
         ]);
+        
+        
+        // return response()->json([
+        //     'email' => $fields['email'],
+        //     'password' => $fields['password']
+        // ]);
+        // print_r( $fields['email'], $fields['password']);
 
         //CHECK IF EMAIL IS REGISTERED IN DB
         $user = User::where('email', $fields['email'])->first();
 
-        if( !$user || Hash::check( $fields['password'], $user->password ) ) {
+        if( !$user || !Hash::check( $fields['password'], $user->password ) ) {
 
             return response()->json([
-                'title' => 'Credenciales invalidas',
-                'message' => 'Por favor revise sus datos e intentelo nuevamente',
+                'ok'        => false,
+                'message'   => 'Por favor revise sus datos e intentelo nuevamente',
             ], 401);
 
         }
@@ -60,10 +67,10 @@ class AuthController extends Controller
         $token = $user->createToken('authtoken')->plainTextToken;
 
         return response()->json([
-            'title' => 'Bienvenido',
-            'message' => 'Continua con tus objetivos' . $user->name,
-            'user' => $user,
-            'token' => $token,
+            'ok'        => true,
+            'message'   => 'Continua con tus objetivos',
+            'user'      => $user,
+            'token'     => $token,
         ], 200);
 
     }
